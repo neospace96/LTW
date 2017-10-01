281aa0fdcdfe600c9fb2b2882806184d2bdcf17c
Dinh Nguyen <nguyendinhteki@gmail.com>
2017-10-02 00:14:12 +0700

    Dinh
    Chinh sua HOME - 12:13AM 02/10/17

--- SadiShop/SadiShop/Controllers/ShopController.cs
+++ SadiShop/SadiShop/Controllers/ShopController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using SadiShop.Models;
 namespace SadiShop.Controllers
 {
     public class ShopController : Controller
     {
+        dbQLQuanAoDataContext data = new dbQLQuanAoDataContext();
+
+        private List<SanPham> Laysanphammoi(int count)
+        {
+            return data.SanPhams.OrderByDescending(a => a.MaSanPham).Take(count).ToList();
+        }
+
         // GET: Shop
         public ActionResult Index()
         {
-            return View();
+            var sanphammoi = Laysanphammoi(3);
+            return View(sanphammoi);
         }
     }
 }
