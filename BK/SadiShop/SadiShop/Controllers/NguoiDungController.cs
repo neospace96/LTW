8ee186575a3ca918c228b9f60ccad9ce8e2cd1a3
Dinh Nguyen <nguyendinhteki@gmail.com>
2017-10-18 18:21:10 +0700

    Gần hoàn tất 06:08PM 18/10/17

---
+++ BK/SadiShop/SadiShop/Controllers/NguoiDungController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SadiShop.Models;
+namespace SadiShop.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        dbQLQuanAoDataContext data = new dbQLQuanAoDataContext();
+        // GET: NguoiDung
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DangKy(FormCollection collection, TaiKhoan tk)
+        {
+            var tendangnhap = collection["username"];
+            var matkhau = collection["password"];
+            var nhaplaimatkhau = collection["repassword"];
+            var hoten = collection["fullname"];
+            var sodienthoai = collection["telephone"];
+            var email = collection["email"];
+            if (String.IsNullOrEmpty(tendangnhap))
+            {
+                ViewData["Error1"] = "Tên đăng nhập không được để trống";
+            }
+            if (String.IsNullOrEmpty(matkhau)){
+                ViewData["Error2"] = "Mật khẩu không được để trống";
+            }
+            if (String.IsNullOrEmpty(nhaplaimatkhau))
+            {
+                ViewData["Error3"] = "Nhập lại mật khẩu không được để trống";
+            }
+            if (String.IsNullOrEmpty(hoten))
+            {
+                ViewData["Error4"] = "Họ tên không được để trống";
+            }
+            if (String.IsNullOrEmpty(sodienthoai))
+            {
+                ViewData["Error5"] = "Số điện thoại không được để trống";
+            }
+            if (String.IsNullOrEmpty(email))
+            {
+                ViewData["Error6"] = "Email không được để trống";
+            }
+            else
+            {
+                tk.Username = tendangnhap;
+                tk.Password = matkhau;
+                tk.HoTen = hoten;
+                tk.DienThoai = sodienthoai;
+                tk.Email = email;
+                data.TaiKhoans.InsertOnSubmit(tk);
+                data.SubmitChanges();
+                return RedirectToAction("DangNhap");
+            }
+            return DangKy();
+        }
+
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection collection)
+        {
+            var tendn = collection["username"];
+            var matkhau = collection["password"];
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Error1"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Error2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                TaiKhoan tk = data.TaiKhoans.SingleOrDefault(n => n.Username == tendn && n.Password == matkhau);
+                if (tk != null)
+                {
+                    Session["TaiKhoan"] = tk;
+                    Session["Ten"] = tk.HoTen;
+                    return RedirectToAction("Index", "Shop");
+                    
+                }
+                else
+                    ViewBag.Thongbao = "Tên đăng nhập hoặc mật khẩu không đúng";
+            }
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            Session["TaiKhoan"] = null;
+            return RedirectToAction("Index", "Shop");
+        }
+
+    }
+}
---
+++ BK/SadiShop/SadiShop/Models/ProductView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SadiShop.Models
+{
+    public class ProductView
+    {
+        public string tensanpham { get; set; }
+        public string tenloai { get; set; }
+        public string masanpham { get; set; }
+        public string manhasanxuat { get; set; }
+        public string thongtin { get; set; }
+        public double giaban { get; set; }
+        public string hinh1 { get; set; }
+        public string hinh2 { get; set; }
+        public string hinh3 { get; set; }
+        public string hinh4 { get; set; }
+        public string hinh5 { get; set; }
+    }
+}
